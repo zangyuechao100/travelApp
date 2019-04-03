ace81c24b68696196b6a0e216c4a4a464f2c7fc5
zangyuechao <markcoder@outlook.com>
2019-04-03 20:58:27 +0800

    add list

--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -2,24 +2,52 @@
   <div>
       <Banner></Banner>
       <DeatilHeader></DeatilHeader>
-      <div class="content"></div>
+      <div class="content">
+        <DeatilList :list="list"></DeatilList>
+      </div>
   </div>
 </template>
 
 <script>
 import Banner from './components/banner.vue'
 import DeatilHeader from './components/header.vue'
+import DeatilList from './components/list.vue'
 export default {
     name: 'Detail',
     components: {
         Banner,
-        DeatilHeader
+        DeatilHeader,
+        DeatilList
+    },
+    data () {
+        return {
+            list: [
+                {
+                    title: '成人票',
+                    children: [{
+                        title: '成人三馆联票',
+                        children: [{
+                            title: '成人三馆联票-连锁店'
+                        }]
+                    }]
+                },
+                {
+                    title: '学生票'
+                },
+                {
+                    title: '儿童票'
+                },
+                {
+                    title: '特惠票'
+                }
+            ]
+        }
     }
 }
 </script>
 
 <style scoped lang="less">
 .content {
-    height: 50rem;
+    height: 20rem;
 }
 </style>
