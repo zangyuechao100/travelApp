1f8c3f4ba54a853235d642bf323cd275536bfd81
zangyuechao <markcoder@outlook.com>
2019-04-03 21:53:13 +0800

    add router

--- src/router.js
+++ src/router.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/home/home.vue'
-import City from './views/city/city.vue'
-import Detail from './views/detail/detail.vue'
 
 Vue.use(Router)
 
@@ -13,17 +10,17 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: () => import('./views/home/home.vue')
     },
     {
       path: '/city',
       name: 'City',
-      component: City
+      component: () => import('./views/city/city.vue')
     },
     {
       path: '/detail/:id',
       name: 'Detail',
-      component: Detail
+      component: () => import('./views/detail/detail.vue')
     }
   ],
   scrollBehavior () {
